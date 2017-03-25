69f73bd8c559da450c4ef5e01f39b8d268c0b658
Xotic750 <xotic750@gmail.com>
2017-03-25 23:01:42 +0100

    Update missed version in docs

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
  *
  * Build a jasmine 1.1.3 spec runner html file.
  *
- * @version 1.0.1
+ * @version 1.0.2
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
